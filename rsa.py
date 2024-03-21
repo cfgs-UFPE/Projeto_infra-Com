c0153214c018b921491fa169490175c0240065c8
CharlysFarias03 <charlesaugustoararaazul@gmail.com>
2024-03-21 16:31:55 -0300

    Atualizei as funções do código de rsa.

--- rsa.py
+++ rsa.py
@@ -1,9 +1,14 @@
 import rsa 
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes as random
+
+"""
+O objetivo das funções encriptar_chave_privada e decriptar_chave_privada é não enviar a chave privada para o computador em plaintext. 
+"""
 
 def encriptar_chave_privada(chave_privada):
     
-    chave = "InfraCom é a melhor cadeira do CIn."
+    chave = random(16)
 
     encriptador = AES.new(chave, AES.MODE_EAX, nonce = nonce)
 
@@ -13,27 +18,24 @@ def encriptar_chave_privada(chave_privada):
 
 def decriptar_chave_privada(chave_privada_encriptada):
 
-    chave = "InfraCom é a melhor cadeira do CIn"
+    chave = random(16)
 
-    decriptador = AES.new('chave', AES.MODE_EAX, nonce = nonce)
+    decriptador = AES.new(chave, AES.MODE_EAX, nonce = nonce)
 
     message = decriptador.decrypt(chave_privada_encriptada)
 
     return message
 
-
 def decriptar(cifra, chave_privada_encriptada):
 
-
-    chave_privada = chave_privada_encriptada
+    chave_privada = decriptar_chave_privada(chave_privada_encriptada)
 
     mensagem = rsa.decrypt(cifra, chave_privada) # aqui o código decripta a cifra usando a chave privada do computador
 
     return mensagem
 
 def encriptar(mensagem, remetente, dest):
-    pass
-
+   
     chave_publica = chaves_dict[dest] # aqui o remetente pega a chave pública do destinatário.
 
     if chave_publica == NULL: # caso o pc ainda não tenha sido registrado na autoridade certificadora.
@@ -42,4 +44,3 @@ def encriptar(mensagem, remetente, dest):
     cifra = rsa.encrypt(mensagem, chave_publica) # aqui o código encripta a mengagem usando a chave pública.
 
     return cifra 
-
