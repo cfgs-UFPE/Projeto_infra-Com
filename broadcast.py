dfb32532579d06ea9a24fe2b89b6e54bd8fddbcb
CharlysFarias03 <charlesaugustoararaazul@gmail.com>
2024-03-22 10:52:20 -0300

    Novas atualizações para o código de broadcast.

--- broadcast.py
+++ broadcast.py
@@ -1,15 +1,19 @@
 import socket
 import sys, time
+import geral
 
-def broadcast(mensagem, porta, computador):
+def broadcast(mensagem, enderecos):
 
-    socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-    socket.bind('', 0)
+    socket = socket(AF_INET, SOCK_DGRAM)
+ 
     socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     
     while True:
-        for _ in range(6):
+        for i in range(len(enderecos.keys())):
 
+            porta = enderecos[i][1]
+            
+            socket.bind(enderecos[i][0], porta)
             sock.sendto(mensagem, (< broadcast >, porta))
             sock.close()
 
